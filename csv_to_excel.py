5dd4a64bc1822569c8524023e1a5fe615023f615
Mehul Verma <mehulverma04@gmail.com>
2023-07-20 13:49:38 +0530

    adding csv to excel

--- csv_to_excel.py
+++ csv_to_excel.py
@@ -1,11 +1,15 @@
-# this is a work in progress
+# requirements
+# pip install pandas openpyxl
 import pandas as pd
+import os
 
 a = input("enter the location of the file with extension: ")
 if os.path.exists(a):
     new_dataFrame = pd.read_csv(a)
     b = input("enter the path where you want to save the pdf: ")
     c = input("enter the name of the pdf that you want to save: ")
-    new_dataFrame.to_excel("SampleExcelFile.xlsx", sheet_name="Subjects", index=False)
+    d = b + "\\"
+    e = d + c + ".xlsx"
+    new_dataFrame.to_excel(e, index=False)
 else:
     print("file does not exist")
